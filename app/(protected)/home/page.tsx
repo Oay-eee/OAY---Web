9864e78f4132440548928f4a2ee7927e99395ab0
FiantsoHarena <fiantsorav@gmail.com>
2025-04-23 17:17:32 +0300

    feat: add a floating dock for navigation

---
+++ app/(protected)/home/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl">Home page</h1>
+  );
+}
---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from "react";
+import {
+  IconBrandX,
+  IconExchange,
+  IconGraph,
+  IconHome,
+  IconLogout,
+  IconUser,
+} from "@tabler/icons-react";
+import { FloatingDock } from "@/components/ui";
+
+export default function SkillPathLayout({ children }: { children: ReactNode }) {
+  const links = [
+    {
+      title: "Home",
+      icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+      href: "/home",
+    },
+
+    {
+      title: "Progression",
+      icon: <IconGraph className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+      href: "/progression",
+    },
+    {
+      title: "Profile",
+      icon: <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+      href: "/profile",
+    },
+    {
+      title: "Additional tabs #1",
+      icon: <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+      href: "/additional-tabs",
+    },
+    {
+      title: "Additional tabs #2",
+      icon: <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+      href: "/additional-tabs",
+    },
+    {
+      title: "Logout",
+      icon: <IconLogout className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+      href: "#",
+    },
+  ];
+
+  return (
+    <main className="relative flex h-screen w-full flex-col items-center justify-center gap-5">
+      {children}
+      <FloatingDock items={links} />
+    </main>
+  );
+}
