2bb3458a9d0d4134c1b2feab4f2b06c90d7a97b5
FiantsoHarena <fiantsorav@gmail.com>
2025-04-23 10:45:34 +0300

    chore: make dark as default theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-slate-50">
+    <main className="flex h-screen w-full items-center justify-center bg-slate-950">
       <Button>Hello from shadcn</Button>
     </main>
   );
