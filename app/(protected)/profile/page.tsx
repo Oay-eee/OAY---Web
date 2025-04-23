e0000a2d3f6592e692571a552ee6ae36ed95451a
FiantsoHarena <fiantsorav@gmail.com>
2025-04-23 17:32:02 +0300

    chore: test profile rendering with avatar and name

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import { useCurrentUser } from "@/hooks";
+
 export default function Profile() {
+  const currentUser = useCurrentUser();
+
+  if (!currentUser?.image) return <h1>No image available</h1>;
+
   return (
-    <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl">Profile page</h1>
+    <main className="flex items-center gap-5">
+      <Image
+        src={currentUser?.image}
+        width={100}
+        height={100}
+        alt="User avatar"
+        className="rounded-full"
+      />
+      <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl">
+        {currentUser?.name}
+      </h1>
+    </main>
   );
 }
