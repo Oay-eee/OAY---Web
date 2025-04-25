e050561850ffd761f81cde04db9223946a834d38
FiantsoHarena <fiantsorav@gmail.com>
2025-04-25 18:11:22 +0300

    chore: add an explicit message for error page

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import { IconExclamationMark } from "@tabler/icons-react";
+import { TextGenerateEffect } from "@/components/aceternity";
 
 export default function AuthErrorPage() {
   return (
     <article>
+      <div className="mb-10 flex flex-col items-center gap-5 text-center">
+        <TextGenerateEffect words="Error :')" />
+      </div>
       <div className="bg-destructive/15 text-destructive flex w-full items-center justify-center gap-x-2 rounded-md p-3 text-sm">
         <IconExclamationMark className="h4 w-4" />
         <p>There is an authentication error</p>
